9a819bc806d5339d7668f4c789124933de717cd1
TienAska <constantine.sky@hotmail.com>
2024-03-30 22:39:42 -0400

    Draw a triangle using mesh shader.
    1. Add a mesh shader.
    2. Update ViewController to support CALayer render loop.

---
+++ Nebula/Shaders.metal
@@ -0,0 +1,41 @@
+//
+//  Shaders.metal
+//  Nebula
+//
+//  Created by Tien Aska on 2024-03-30.
+//
+
+#include <metal_stdlib>
+
+struct VertexData    { float4 position [[position]]; };
+struct PrimitiveData { float4 color; };
+
+using triangle_mesh_t = metal::mesh<
+                                    VertexData,               // Vertex type
+                                    PrimitiveData,            // Primitive type
+                                    3,                       // Maximum vertices
+                                    1,                        // Maximum primitives
+                                    metal::topology::triangle // Topology
+>;
+
+[[mesh]]
+void mesh(triangle_mesh_t outputMesh, uint tid [[thread_index_in_threadgroup]]) {
+    outputMesh.set_vertex(0, VertexData{float4(0.0f, 0.0f, 0.0f, 1.0f)});
+    outputMesh.set_index(0, 0);
+    outputMesh.set_vertex(1, VertexData{float4(1.0f, 0.0f, 0.0f, 1.0f)});
+    outputMesh.set_index(1, 1);
+    outputMesh.set_vertex(2, VertexData{float4(0.0f, 1.0f, 0.0f, 1.0f)});
+    outputMesh.set_index(2, 2);
+    outputMesh.set_primitive(0, PrimitiveData{float4(1.0f, 0.3f, 0.1f, 1.0f)});
+    outputMesh.set_primitive_count(1);
+}
+
+struct FragmentData {
+    VertexData vert;
+    PrimitiveData prim;
+};
+
+[[fragment]]
+float4 frag(FragmentData in [[stage_in]]) {
+    return in.prim.color;
+}
--- Nebula/ViewController.swift
+++ Nebula/ViewController.swift
@@ -6,13 +6,76 @@
 //
 
 import UIKit
+import Metal
 
 class ViewController: UIViewController {
-
+    var device: MTLDevice!
+    var metalLayer: CAMetalLayer!
+    var pipelineState: MTLRenderPipelineState!
+    var commandQueue: MTLCommandQueue!
+    var timer: CADisplayLink!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        device = MTLCreateSystemDefaultDevice()
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+        
+        let defaultLibrary = device.makeDefaultLibrary()!
+        let meshProgram = defaultLibrary.makeFunction(name: "mesh")
+        let fragProgram = defaultLibrary.makeFunction(name: "frag")
+        
+        
+        let pipelineStateDesc = MTLMeshRenderPipelineDescriptor()
+        pipelineStateDesc.meshFunction = meshProgram
+        pipelineStateDesc.fragmentFunction = fragProgram
+        pipelineStateDesc.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        let pipelineOption = MTLPipelineOption()
+        
+        let ret = try! device.makeRenderPipelineState(descriptor: pipelineStateDesc, options: pipelineOption)
+        pipelineState = ret.0
+        
+        commandQueue = device.makeCommandQueue()
+        
+        timer = CADisplayLink(target: self, selector: #selector(gameloop))
+        timer.add(to: RunLoop.main, forMode: .default)
+        
     }
+    
+    func render() {
+        guard let drawable = metalLayer?.nextDrawable() else { return }
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(
+          red: 0.0,
+          green: 104.0/255.0,
+          blue: 55.0/255.0,
+          alpha: 1.0)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()!
+
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
+        renderEncoder.setRenderPipelineState(pipelineState)
+        renderEncoder.drawMeshThreads(MTLSizeMake(1, 1, 1), threadsPerObjectThreadgroup: MTLSizeMake(1, 1, 1), threadsPerMeshThreadgroup: MTLSizeMake(1, 1, 1))
+        renderEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+
+    @objc func gameloop() {
+      autoreleasepool {
+        self.render()
+      }
+    }
+
 
 
 }
